8ec346d6c0684afb20e5668d859782a47af5c1f6
Abdullah R Alanazi <x.3body@hotmail.com>
2022-01-09 14:47:59 +0300

    auth login

--- resources/views/components/head/tinymce-config.blade.php
+++ resources/views/components/head/tinymce-config.blade.php
@@ -1,5 +1,5 @@
 <div>
-   <script src="{{ asset('js/tinymce/tinymce.min.js') }}" referrerpolicy="origin"></script>
+   <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <script>
         tinymce.init({
          selector: 'textarea#myeditorinstance', // Replace this CSS selector to match the placeholder element for TinyMCE
